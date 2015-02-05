762e412df8c6891650ea419b5066b673429d8d13
Juan Luis <juanluisrp@geocat.net>
2015-02-05 16:02:07 +0100

    Changes in Metadata preview style

--- web-ui/src/main/resources/catalog/js/search/OGPSearchController.js
+++ web-ui/src/main/resources/catalog/js/search/OGPSearchController.js
@@ -173,4 +173,19 @@ function($scope, $http, $sce, $modalInstance, document) {
 
 }]);
 
+module.directive('iframeSetDimentionsOnload', [function(){
+       return {
+           restrict: 'A',
+           link: function(scope, element, attrs){
+               element.on('load', function(){
+                   /* Set the dimensions here,
+                      I think that you were trying to do something like this: */
+                      var iFrameHeight = element[0].contentWindow.document.body.scrollHeight + 'px';
+                      var iFrameWidth = '100%';
+                      element.css('width', iFrameWidth);
+                      element.css('height', iFrameHeight);
+               })
+           }
+       }}])
+
 })();
