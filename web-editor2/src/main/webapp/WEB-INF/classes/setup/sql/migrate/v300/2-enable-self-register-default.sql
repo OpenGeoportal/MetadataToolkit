070a76190901f1e428cc5304a904557be0d86cea
Juan Luis <juanluisrp@geocat.net>
2015-03-31 10:19:09 +0200

    Fix view name

--- web-editor2/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v300/2-enable-self-register-default.sql
+++ web-editor2/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v300/2-enable-self-register-default.sql
@@ -1,2 +1,2 @@
 UPDATE Settings SET value='true' WHERE name='system/userSelfRegistration/enable';
-UPDATE Settings SET value='ogp'  WHERE name = 'system/metadata/defaultView';
+UPDATE Settings SET value='ogp'  WHERE name = 'system/ui/defaultView';
